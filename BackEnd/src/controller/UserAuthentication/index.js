da74a69af20e9a7817117e0be64a4e9b79fd7c8a
Maryam Saba <maryams91101@gmail.com>
2024-08-29 18:56:18 +0500

    updation

---
+++ BackEnd/src/controller/UserAuthentication/index.js
@@ -0,0 +1,162 @@
+import userModel from "../../model/userModel.js";
+import { hash, compare } from "bcrypt";
+
+const registerController = {
+  // Register a new user
+  register: async (req, res) => {
+    try {
+      const { name, email, password, phone_number } = req.body;
+
+      console.log(req.body);
+
+      // Check if the user already exist
+      const userCheck = await userModel.findOne({ email });
+      if (userCheck) {
+        return res.status(409).json({ message: "User already exists!" });
+      }
+
+      // Hash the password before saving
+      const hashedPassword = await hash(password, 10);
+
+      // Create a new user with hashed password
+      const userRegister = await userModel.create({
+        name,
+        email,
+        phone_number,
+        password: hashedPassword,
+      });
+      await userRegister.save();
+      res
+        .status(201)
+        .json({ message: "User registered successfully", userRegister });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  getAllUser: async (req, res) => {
+    try {
+      const employees = await userModel.find();
+      res.json(employees);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  updateUser : async(req,res) => {
+    try {
+      const id = req.params.id;
+      const updateData = req.body;
+      //Update Data
+      //findIdAndUpdate is an atomic method to update data, new:true means it return the updated data
+      const employees = await userModel.findByIdAndUpdate(id,updateData,{new:true});
+      if(!employees)
+      {
+        return res.status(404).json({Warning : `Employee is not found`});
+      }
+      
+      res.status(200).json({Success: `Employee updated successfully`, Updation : employees});
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  deleteUser : async(req,res) => {
+    try {
+      const {id} = req.params;
+      // const deleteUser = req.body;
+      const DeleteUser = await userModel.findByIdAndDelete(id);
+      if(!DeleteUser) {
+        return res.status(404).json({Warning : "Data not found"})
+      }
+      res.status(200).json({Success: "Employee is deleted!", Deletion : DeleteUser})
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  // User login
+  // login: async (req, res) => {
+  //   try {
+  //     const { email, password } = req.body;
+  //     console.log("body:", req.body);
+
+  //     // Find the user by email
+  //     const userCheck = await userModel.findOne({ email });
+
+  //     console.log("UserCheck", userCheck);
+  //     if (!userCheck) {
+  //       return res.status(404).json({ message: "Invalid Credentials!" });
+  //     }
+
+  //     // Compare the provided password with the stored hashed password
+  //     const isPasswordMatch = await compare(password, userCheck.password);
+
+  //     if (!isPasswordMatch) {
+  //       return res.status(404).json({ message: "Invalid Credentials!" });
+  //     }
+
+  //      // Update loginTime to the current date and time
+  //   userCheck.loginTime = new Date();
+
+  //   // Save the updated user info
+  //   await userCheck.save(); 
+
+  //     const data = {
+  //       id: userCheck._id,
+  //       email: userCheck.email,
+  //       loginTime : userCheck.loginTime,
+  //       // Do not send the password back in the response
+  //     };
+
+  //     res.status(200).json({ message: "Login successful", data });
+  //   } catch (error) {
+  //     console.error("Error in login:", error);
+  //     if (!res.headersSent) {
+  //       return res.status(500).json({ message: "Internal Server Error!" });
+  //     }
+  //   }
+  // },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      console.log("body:", req.body);
+  
+      // Find the user by email
+      const userCheck = await userModel.findOne({ email });
+  
+      console.log("UserCheck", userCheck);
+      if (!userCheck) {
+        return res.status(404).json({ message: "Invalid Credentials!" });
+      }
+  
+      // Compare the provided password with the stored hashed password
+      const isPasswordMatch = await compare(password, userCheck.password);
+  
+      if (!isPasswordMatch) {
+        return res.status(404).json({ message: "Invalid Credentials!" });
+      }
+  
+      // Update loginTime to the current date and time
+      userCheck.loginTime = new Date();
+  
+      // Save the updated user info
+      await userCheck.save();
+  
+      const data = {
+        id: userCheck._id,
+        email: userCheck.email,
+        // loginTime: userCheck.loginTime, // Include loginTime in the response if needed
+      };
+  
+      res.status(200).json({ message: "Login successful", data });
+    } catch (error) {
+      console.error("Error in login:", error);
+      if (!res.headersSent) {
+        return res.status(500).json({ message: "Internal Server Error!" });
+      }
+    }
+  },
+  
+};
+
+export default registerController;
--- BackEnd/src/routes/userRoutes/index.js
+++ BackEnd/src/routes/userRoutes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import registerController from "../../controller/Register/index.js";
+import registerController from "../../controller/UserAuthentication/index.js";
 
 const userRouter = Router();
 
 userRouter.post("/Register", registerController.register);
 userRouter.get("/employees", registerController.getAllUser);
+userRouter.put("/UpdateEmployee/:id", registerController.updateUser);
+userRouter.delete("/DeleteEmployee/:id", registerController.deleteUser);
 userRouter.post("/Login", registerController.login);
 
 export default userRouter;
