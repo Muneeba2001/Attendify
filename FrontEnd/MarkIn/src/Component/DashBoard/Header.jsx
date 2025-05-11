d936615530ab8a82869e06f2be6f099631921b0a
Muneeba2001 <muneebawaseem1006@gmail.com>
2025-05-11 17:05:05 +0500

    employee loginUI updated

--- FrontEnd/MarkIn/src/Component/DashBoard/Header.jsx
+++ FrontEnd/MarkIn/src/Component/DashBoard/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div>
       <nav className="relative z-10 flex justify-between items-center rounded-sm bg-[#1e1e1e] text-white p-2 pl-10 shadow-lg">
         <h1 className="text-2xl font-bold">
-          <NavLink to="/Home">Attendify</NavLink>
+          <NavLink to="/">Attendify</NavLink>
         </h1>
 
         <div className="flex items-center gap-6 mr-6">
